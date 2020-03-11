ec02eddccc6ae41c4eb86c9412dafadd6c81ab45
Linsin <jianglingxin@aliyun.com>
2020-03-11 12:05:44 +0800

    fix: geographic

---
+++ include/fusion_mapping/core/sensor_data/gnss_data.h
@@ -0,0 +1,39 @@
+//
+// Created by linsin on 13/02/2020.
+//
+
+#ifndef FUSION_MAPPING_INCLUDE_SENSOR_DATA_GNSS_DATA_H_
+#define FUSION_MAPPING_INCLUDE_SENSOR_DATA_GNSS_DATA_H_
+
+#include <vector>
+#include <string>
+
+#include "fusion_mapping/core/Geocentric/LocalCartesian.hpp"
+
+using std::vector;
+using std::string;
+
+namespace FM{
+class GNSSData {
+ public:
+  double time = 0.0;
+  double longitude = 0.0;
+  double latitude = 0.0;
+  double altitude = 0.0;
+  double local_E = 0.0;
+  double local_N = 0.0;
+  double local_U = 0.0;
+  int status = 0;
+  int service = 0;
+
+ private:
+  static GeographicLib::LocalCartesian geo_converter;
+  static bool origin_position_inited;
+
+ public:
+  void InitOriginPosition();
+  void UpdateXYZ();
+};
+}
+
+#endif //FUSION_MAPPING_INCLUDE_SENSOR_DATA_GNSS_DATA_H_
---
+++ include/fusion_mapping/core/subscriber/gnss_subscriber.h
@@ -0,0 +1,32 @@
+//
+// Created by linsin on 13/02/2020.
+//
+
+#ifndef FUSION_MAPPING_INCLUDE_SUBSCRIBER_GNSS_SUBSCRIBER_H_
+#define FUSION_MAPPING_INCLUDE_SUBSCRIBER_GNSS_SUBSCRIBER_H_
+
+
+#include <deque>
+#include <ros/ros.h>
+#include "sensor_msgs/NavSatFix.h"
+#include "fusion_mapping/core/sensor_data/gnss_data.h"
+
+namespace FM {
+class GNSSSubscriber {
+ public:
+  GNSSSubscriber(ros::NodeHandle& nh, std::string topic_name, size_t buff_size);
+  GNSSSubscriber() = default;
+  void ParseData(std::deque<GNSSData>& deque_gnss_data);
+
+ private:
+  void msg_callback(const sensor_msgs::NavSatFixConstPtr& nav_sat_fix_ptr);
+
+ private:
+  ros::NodeHandle nh_;
+  ros::Subscriber subscriber_;
+
+  std::deque<GNSSData> new_gnss_data_;
+};
+}
+
+#endif //FUSION_MAPPING_INCLUDE_SUBSCRIBER_GNSS_SUBSCRIBER_H_
