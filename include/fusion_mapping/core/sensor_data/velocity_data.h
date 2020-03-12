9cbbf102d15b75dbd646cce8058c2fe60452c8b9
Linsin <jianglingxin@aliyun.com>
2020-03-12 15:57:34 +0800

    feat: velocity transform coordinate

--- include/fusion_mapping/core/sensor_data/velocity_data.h
+++ include/fusion_mapping/core/sensor_data/velocity_data.h
@@ -29,6 +29,8 @@ class VelocityData {
 
  public:
   static bool SyncData(std::deque<VelocityData>& UnsyncedData, std::deque<VelocityData>& SyncedData, double sync_time);
+
+  void TransformCoordinate(Eigen::Matrix4f transform_matrix);
 };
 }
 
