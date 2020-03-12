b61ed34fc8a52b2b69e1bb2e901530beda471484
Linsin <jianglingxin@aliyun.com>
2020-03-12 13:50:01 +0800

    feat: imu sync

--- include/fusion_mapping/core/sensor_data/imu_data.h
+++ include/fusion_mapping/core/sensor_data/imu_data.h
@@ -7,6 +7,8 @@
 
 #include <Eigen/Dense>
 
+#include <deque>
+#include <cmath>
 namespace FM {
 class IMUData {
  public:
@@ -22,11 +24,21 @@ class IMUData {
     double z = 0.0;
   };
 
-  struct Orientation {
+  class Orientation {
+   public:
     double x = 0.0;
     double y = 0.0;
     double z = 0.0;
     double w = 0.0;
+
+   public:
+    void Normlize() {
+      double norm = sqrt(pow(x, 2.0) + pow(y, 2.0) + pow(z, 2.0) + pow(w, 2.0));
+      x /= norm;
+      y /= norm;
+      z /= norm;
+      w /= norm;
+    }
   };
 
   double time = 0.0;
@@ -36,13 +48,8 @@ class IMUData {
 
  public:
   // 把四元数转换成旋转矩阵送出去
-  Eigen::Matrix3f GetOrientationMatrix() {
-    Eigen::Quaterniond q(orientation.w, orientation.x, orientation.y, orientation.z);
-    Eigen::Matrix3f matrix = q.matrix().cast<float>();
-
-    return matrix;
-  }
+  Eigen::Matrix3f GetOrientationMatrix();
+  static bool SyncData(std::deque<IMUData>& UnsyncedData, std::deque<IMUData>& SyncedData, double sync_time);
 };
 }
-
 #endif //FUSION_MAPPING_INCLUDE_SENSOR_DATA_IMU_DATA_H_
